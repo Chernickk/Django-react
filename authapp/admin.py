1125e71244482900721ee07e522dc94e4949e250
nikita cherenkov <n.cherenkov@qiwi.com>
2023-08-11 13:03:49 +0300

    Update package.json

--- authapp/admin.py
+++ authapp/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import DiaryUser, UserSettings, UserWhiteList
 
 admin.site.register(DiaryUser)
+admin.site.register(DiaryUser)
+
 admin.site.register(UserSettings)
 admin.site.register(UserWhiteList)
